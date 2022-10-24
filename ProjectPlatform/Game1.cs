81070c7b3c499860958fa18b16e4c3b08fd7acc9
Temptica <Nicovsgaming@gmail.com>
2022-10-24 14:22:46 +0200

    Add idle animation

--- ProjectPlatform/Game1.cs
+++ ProjectPlatform/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace ProjectPlatform
 {
@@ -8,44 +9,78 @@ namespace ProjectPlatform
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        List<IMovable> Movables;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            Movables = new List<IMovable>();
+            // _graphics.IsFullScreen = true;
+            ;
+            _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            
             // TODO: use this.Content to load your game content here
+            Movables.Add(new Otter(Content.Load<Texture2D>("Otterly Idle"), new Vector2(100, 100)));
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            #region Controlls
+            
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
+            KeyboardState keyState = Keyboard.GetState();
+            if (keyState.IsKeyDown(Keys.D))
+                _ = "";//Right
+            if (keyState.IsKeyDown(Keys.Q))
+                _ = "";//Left
+            if (keyState.IsKeyDown(Keys.Z) )
+                _ = ""; //jump
+            if (keyState.IsKeyDown(Keys.S))
+                _ = ""; //sleep
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+               //attack
+            }
 
+            #endregion
             // TODO: Add your update logic here
-
+            foreach(IMovable movable in Movables)
+            {
+                movable.Update(gameTime);
+            }
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            _spriteBatch.Begin();
             // TODO: Add your drawing code here
-
+            foreach (IMovable movable in Movables)
+            {
+                movable.Draw(_spriteBatch);
+            }
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ ProjectPlatform/Otter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectPlatform
+{
+    enum state { Idle, Walking, Running, Jumping, Attacking, Sleeping, Dead }
+    internal class Otter : IMovable
+    {
+        public Vector2 Velocity { get; set; }
+        public Vector2 Position { get; set; }
+        public Texture2D Texture { get; set; }
+        public Rectangle Rectangle { get; set; }
+        public int MoveSpeed { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public int AttackRange { get; set; }
+        public state State { get; set; }
+        int fps = 4;
+        int height = 200;
+        int width = 200;
+        int currentx;
+        double time = 0d;
+
+        public Otter(Texture2D otter, Vector2 position)
+        {
+            Texture = otter;
+            Position = position;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (State == state.Idle)
+            {
+                //do idle stuff
+                //make idle animation
+                time += gameTime.ElapsedGameTime.TotalMilliseconds; 
+
+                if (time > 1000/fps)
+                {
+                    currentx += width;
+                    if (currentx >= Texture.Width)
+                        currentx = 0;
+                    time = 0;
+                }
+
+            }
+            Position += Velocity;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position,new Rectangle(currentx,0,height,width),Color.White,0,Vector2.Zero,0.5f,SpriteEffects.None,0f);
+        }
+        
+        
+    }
+}
