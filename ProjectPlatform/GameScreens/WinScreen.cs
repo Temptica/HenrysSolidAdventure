a19451ccf753217b20534bfef906f8601e92ceeb
Nicolas Vanruysseveldt <Nic.Vanruy@gmail.com>
2022-12-29 12:03:24 +0100

    Add Winning screen

---
+++ ProjectPlatform/GameScreens/WinScreen.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using OtterlyAdventure.Controller;
+using OtterlyAdventure.Graphics;
+
+namespace OtterlyAdventure.GameScreens
+{
+    internal class WinScreen: IGameScreen
+    {
+        private List<Text> _texts;
+        private List<Button> _buttons;
+        Screen _screen;
+
+        public WinScreen(Screen screen, SpriteFont font, ContentManager content)
+        {
+            _screen = screen;
+            var halfWidth = screen.Width / 2f;
+            var halfHeight = screen.Height / 2f;
+            string title = "You won!";
+            var length = font.MeasureString(title).Length();
+            Vector2 textPosition = new(halfWidth - length / 2, screen.Height / 10f);
+
+            _texts = new List<Text> {
+                new(textPosition,title , Color.Red, 1f, 0f, font)
+            };
+            var startTexture = content.Load<Texture2D>("buttons/EmptyButton");
+            _buttons = new List<Button>
+            {
+                new("Menu", startTexture, new Vector2((_screen.Width - startTexture.Width) / 2f, (_screen.Height - startTexture.Height) / 2f), "Menu"),
+                new("Play again", startTexture, new Vector2((_screen.Width - startTexture.Width) / 2f, (_screen.Height - startTexture.Height) / 2f+startTexture.Height + 20f), "Restart")
+
+            };
+            AudioController.Instance.PlaySong("MainMenu");
+        }
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Sprites sprites)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
